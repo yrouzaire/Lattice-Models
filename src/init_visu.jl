df87cea94649cb69d45052d7dbec8d59aac47f70
yrouzaire <rouzaire.ylann@gmail.com>
2022-08-26 12:45:05 +0200

    manually created vortices (single, pair, 2pairs)

--- src/init_visu.jl
+++ src/init_visu.jl
@@ -3,22 +3,59 @@ include("models.jl");
 using Plots,ColorSchemes,LaTeXStrings
 
 ## ------------------------ Initializations  ------------------------
-function init_thetas!(model::AbstractModel,space::AbstractLattice,init::String)
+function init_thetas!(model::AbstractModel,space::AbstractLattice;init::String,kwargs...)
     @assert model.L == space.L
     L = model.L
-    # model.symmetry == "polar" ? symm = 2 : symm = 1
-    if init == "hightemp" || "disorder"
+    if init in ["hightemp" , "disorder"]
         model.thetas = 2π*rand(L,L)
-    elseif init == "lowtemp" || "polar_order"
+    elseif init in ["lowtemp" , "polar_order"]
         model.thetas = zeros(L,L)
-    elseif init == "lowtemp_nematic" || "nematic_order"
+    elseif init in ["lowtemp_nematic" , "nematic_order"]
         model.thetas = rand(Bool,L,L)*π
-    elseif init == "isolated"  thetas = create_single_defect(args...,L,round(Int,L/2),round(Int,L/2)) # in case of something more exotic, recall that the use is : create_single_defect(q,type,L,y0,x0) (x and y swapped)
-    elseif init == "pair"      thetas = create_pair_vortices(L,r0,args...)
-    elseif init == "2pair"     thetas = create_pair_vortices(L,r0,args...) + create_pair_vortices(L,round(Int,r0/2),args...)'
+    elseif init in ["isolated" , "single"]
+        model.thetas = create_single_defect(L,round(Int,L/2),round(Int,L/2);kwargs...) # in case of something more exotic, recall that the use is : create_single_defect(q,type,L,y0,x0) (x and y swapped)
+    elseif init == "pair"
+        model.thetas = create_pair_vortices(L;kwargs...)
+    elseif init in ["2pairs" , "2pair"]
+        model.thetas = create_2pairs_vortices(L;kwargs...)
     else error("ERROR : Type of initialisation unknown. Choose among \"hightemp/order\",\"lowtemp/polar_order\",\"isolated\" , \"pair\" , \"2pair\" or \"lowtemp_nematic/nematic_order\" .")
     end
     return nothing
+end
+
+function create_single_defect(L,x0=round(Int,L/2),y0=round(Int,L/2);q=1,type="source")
+    @assert (q > 0 && type in ["source","sink","clockwise","counterclockwise"]) || (q < 0 && type in ["convergent","divergent","threefold1","threefold2"])
+    thetas = zeros(Float32,L,L)
+    for y in 1:L , x in 1:L
+        # q > 0
+        if     type == "clockwise"            thetas[x,y] = q * atan(y-y0,x-x0)
+        elseif type == "counterclockwise"     thetas[x,y] = q * atan(y-y0,x-x0) + pi
+        elseif type == "sink"                 thetas[x,y] = q * atan(y-y0,x-x0) + pi/2
+        elseif type == "source"               thetas[x,y] = q * atan(y-y0,x-x0) - pi/2
+        # q = -1/2 (when q = -1, I think they are all equivalent)
+        elseif type == "threefold1"           thetas[x,y] = q * atan(y-y0,x-x0)
+        elseif type == "threefold2"           thetas[x,y] = q * atan(y-y0,x-x0) + pi
+        elseif type == "divergent"            thetas[x,y] = q * atan(y-y0,x-x0) + pi/2
+        elseif type == "convergent"           thetas[x,y] = q * atan(y-y0,x-x0) - pi/2
+        end
+    end
+    return thetas
+end
+
+function create_pair_vortices(L;r0=Int(L/2),q,type)
+    #= Check for meaningfulness of the defaults separation,
+    otherwise the defaults will annihilate with relaxation =#
+    @assert r0 ≤ 0.5L  "Error : r0 > L/2. "
+    @assert iseven(r0) "Error : r0 has to be even. "
+
+    thetas = create_single_defect(L,round(Int,L/2+r0/2),round(Int,L/2),q=+q,type=type[1]) +
+             create_single_defect(L,round(Int,L/2-r0/2),round(Int,L/2),q=-q,type=type[2])
+    return thetas
+end
+
+function create_2pairs_vortices(L;r0,q,type)
+    @assert isinteger(r0/4) "R0 has to be a multiple of 4 !"
+    return create_pair_vortices(L;r0,q,type) + create_pair_vortices(L;r0=r0/2,q,type)'
 end
 
 ## ------------------------ Visualization  ------------------------
--- tests/test_init_visu.jl
+++ tests/test_init_visu.jl
@@ -1,7 +1,11 @@
+include("../src/init_visu.jl")
 pyplot(box=true,fontfamily="sans-serif",label=nothing,palette=ColorSchemes.tab10.colors[1:10],grid=false,markerstrokewidth=0,linewidth=1.3,size=(400,400),thickness_scaling = 1.5)
 cols = cgrad([:black,:blue,:green,:orange,:red,:black]);
-plot()
+plot(rand(10))
+
+
 L = 200
 model1 = XY(L,0.1,"polar")
 lattice = TriangularLattice(L)
-init_thetas!(model1,lattice,"hightemp")
+init_thetas!(model1,lattice,"single")
+plot_theta(model1,lattice)
