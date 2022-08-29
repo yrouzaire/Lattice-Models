5cc8c3ba8b411f7059c95c7354379f6b4083a370
yrouzaire <rouzaire.ylann@gmail.com>
2022-08-29 15:53:28 +0200

    Some more plot tests

--- tests/test_init_visu.jl
+++ tests/test_init_visu.jl
@@ -1,19 +1,48 @@
 include("../src/init_visu.jl");
 
 # Physical Parameters
-L = 200
+L = 300
     T = 0.1
     symmetry = "polar"
     Var = 0.1
     vision = π
     params_phys = Dict("L"=>L,"T"=>T,"Var"=>Var,"vision"=>vision,"symmetry"=>symmetry)
+    
 # Numerical Parameters
 dt = 1E-2
     float_type = Float32
     params_num  = Dict("dt"=>dt,"float_type"=>float_type)
 
-model = VisionXY(params_phys,params_num)
+model = XY(params_phys,params_num)
 lattice = TriangularLattice(L)
 init_thetas!(model,lattice,init="2pair",q=1,r0=60,type=["source","divergent"])
-# init_thetas!(model,lattice,init="isolated",q=1,type="source")
     plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="isolated",q=1,type="source")
+    plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="isolated",q=-1,type="convergent")
+    plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="isolated",q=1/2,type="source")
+    plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="pair",q=1,r0=60,type=["source","divergent"])
+    plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="2pair",q=1,r0=60,type=["source","divergent"])
+    plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="pair",q=1/2,r0=60,type=["source","divergent"])
+    plot_theta(model,lattice)
+&
+
+init_thetas!(model,lattice,init="2pair",q=1/2,r0=60,type=["source","divergent"])
+    plot_theta(model,lattice)
+&
