54e03957a253703010ae07a946183452bb3fc0b8
yrouzaire <rouzaire.ylann@gmail.com>
2022-08-29 15:48:22 +0200

    repercute les chgts

--- tests/test_core_methods.jl
+++ tests/test_core_methods.jl
@@ -1,8 +1,19 @@
 include("../src/core_methods.jl");
+include("../src/init_visu.jl");
 
 ## Tests get neighbours
+# Physical Parameters
 L = 200
-model = XY(L,0.1,"polar")
+    T = 0.1
+    symmetry = "polar"
+    params_phys = Dict("L"=>L,"T"=>T,"symmetry"=>symmetry)
+
+# Numerical Parameters
+dt = 1E-2
+    float_type = Float32
+    params_num  = Dict("dt"=>dt,"float_type"=>float_type)
+
+model = XY(params_phys,params_num)
 lattice = TriangularLattice(L)
 init_thetas!(model,lattice,init="hightemp")
 plot_theta(model,lattice)
