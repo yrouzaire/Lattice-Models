4ac3d55df0b3058a7db9ac9fc9c7992182db745e
yrouzaire <rouzaire.ylann@gmail.com>
2022-08-30 13:06:09 +0200

    Takes care of non periodic lattices

--- src/core_methods.jl
+++ src/core_methods.jl
@@ -3,7 +3,6 @@ include("models.jl");
 
 ## ------------------------ Get Neighbours ------------------------
 # NOTE : no need to define get_neighbours(model::AbstractModel,lattice::AbstractLattice ...)
-# TODO deal with non periodic BC
 function get_neighbours(thetas::Matrix{<:T},model::AbstractModel{T},lattice::TriangularLattice,i::Int,j::Int,bulk::Bool=false)::Vector{T} where T<:AbstractFloat
     L = lattice.L
 
@@ -85,10 +84,12 @@ end
 # end
 
 function sum_influence_neighbours(theta::T,angles_neighbours::Vector{<:T},model::AbstractModel{T},lattice::AbstractLattice)::T where T<:AbstractFloat
-    return sum(sin,angles_neighbours .- theta)
+    # default case, nothing to worry about
+    return sum(sin,angles_neighbours .- theta,init=0) # init = 0 in case angles_neighbours is empty
 end
 
-function sum_influence_neighbours(theta::T,angles_neighbours::Vector{<:T},model::VisionXY{T},lattice::TriangularLattice)::T where T<:AbstractFloat
+
+function sum_influence_neighbours(theta::T,angles_neighbours::Vector{<:T},model::VisionXY{T},lattice::TriangularLattice{true})::T where T<:AbstractFloat
     weights  = zeros(T,length(angles_neighbours))
     symm     = sym(model)
     theta0   = mod(theta,symm)
@@ -104,17 +105,15 @@ function sum_influence_neighbours(theta::T,angles_neighbours::Vector{<:T},model:
         end
     end
 
-    return sum(sin.(angles_neighbours .- theta) .* weights)
+    return sum(sin.(angles_neighbours .- theta) .* weights , init = 0) # init = 0 in case angles_neighbours is empty
 end
 
 
 ## ------------------------ Update ------------------------
 # NOTE : no need to define update!(model::AbstractModel,lattice::AbstractLattice)
 function update!(thetas::Matrix{<:T},model::AbstractModel,lattice::AbstractLattice,tmax::Number) where T<:AbstractFloat
-    niter = 1
-    while model.t < tmax && niter<1E4
+    while model.t < tmax
         update!(thetas,model,lattice)
-        niter += 1
     end
     return thetas
 end
@@ -136,15 +135,17 @@ function update!(thetas::Matrix{<:FT},model::Union{XY{FT},VisionXY{FT}},lattice:
 
     # On the borders
     ij_in_bulk = false
-    for j in [1,L] , i in 1:L
-        θ = thetas_old[i,j]
-        angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
-        thetas[i,j] =  θ + dt*sum_influence_neighbours(θ,angle_neighbours,model,lattice) + sqrt(2T*dt)*randn(FT)
-    end
-    for j in 2:L-1 , i in [1,L]
-        θ = thetas_old[i,j]
-        angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
-        thetas[i,j] =  θ + dt*sum_influence_neighbours(θ,angle_neighbours,model,lattice) + sqrt(2T*dt)*randn(FT)
+    if lattice.periodic # if not periodic, juste leave the edge unchanged, otherwise get_neighbours too complicated
+        for j in [1,L] , i in 1:L
+            θ = thetas_old[i,j]
+            angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
+            thetas[i,j] =  θ + dt*sum_influence_neighbours(θ,angle_neighbours,model,lattice) + sqrt(2T*dt)*randn(FT)
+        end
+        for j in 2:L-1 , i in [1,L]
+            θ = thetas_old[i,j]
+            angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
+            thetas[i,j] =  θ + dt*sum_influence_neighbours(θ,angle_neighbours,model,lattice) + sqrt(2T*dt)*randn(FT)
+        end
     end
 
     model.t += dt
@@ -168,15 +169,17 @@ function update!(thetas::Matrix{<:FT},model::AXY{FT},lattice::AbstractLattice) w
 
     # On the borders
     ij_in_bulk = false
-    for j in [1,L] , i in 1:L
-        θ = thetas_old[i,j]
-        angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
-        thetas[i,j] =  θ + dt*(model.omegas[i,j] + sum(sin,angle_neighbours .- θ)) + sqrt(2T*dt)*randn(FT)
-    end
-    for j in 2:L-1 , i in [1,L]
-        θ = thetas_old[i,j]
-        angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
-        thetas[i,j] =  θ + dt*(model.omegas[i,j] + sum(sin,angle_neighbours .- θ)) + sqrt(2T*dt)*randn(FT)
+    if lattice.periodic # if not periodic, juste leave the edge unchanged, otherwise get_neighbours too complicated
+        for j in [1,L] , i in 1:L
+            θ = thetas_old[i,j]
+            angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
+            thetas[i,j] =  θ + dt*(model.omegas[i,j] + sum(sin,angle_neighbours .- θ)) + sqrt(2T*dt)*randn(FT)
+        end
+        for j in 2:L-1 , i in [1,L]
+            θ = thetas_old[i,j]
+            angle_neighbours = get_neighbours(thetas_old,model,lattice,i,j,ij_in_bulk)
+            thetas[i,j] =  θ + dt*(model.omegas[i,j] + sum(sin,angle_neighbours .- θ)) + sqrt(2T*dt)*randn(FT)
+        end
     end
 
     model.t += dt
