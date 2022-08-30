07c08cc4a1f047a961d1a2088b03edd9a72d7bbd
yrouzaire <rouzaire.ylann@gmail.com>
2022-08-30 11:53:07 +0200

    Some tests for dispatch on parametric types

--- tests/test_defects.jl
+++ tests/test_defects.jl
@@ -20,3 +20,33 @@ thetas = init_thetas(lattice,init="isolated",q=1,type="source")
 # thetas = init_thetas(lattice,init="pair",q=1,r0=60,type=["source","divergent"])
 spot_defects(thetas,model,lattice)
 plot_theta(thetas,model,lattice,defects=true)
+
+
+##
+abstract type AbstractTest{A,B} end
+mutable struct X{A,B} <: AbstractTest{A,B}
+    p1
+    p2
+end
+function X(A,B)
+    return X{A,B}(1,1)
+end
+x = X(true,true)
+typeof(x)
+function f1(x::AbstractTest{A,B}) where {A=Bool,B}
+    return "A = true ; B = true"
+end
+function f2(x::X{A,B}) where {A,B}
+    return "General"
+end
+function f2(x::X{true,true}) where {A,B}
+    return "true true"
+end
+function f2(x::X{A,false}) where {A,B}
+    return "A false"
+end
+
+f2(X(false,false))
+
+
+f1(x)
