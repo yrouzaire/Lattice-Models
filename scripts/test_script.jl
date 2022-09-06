979f1adf3432879737934c09f4583dc1a631f8ff
yrouzaire <rouzaire.ylann@gmail.com>
2022-09-06 17:01:09 +0200

    qqch

--- scripts/test_script.jl
+++ scripts/test_script.jl
@@ -1,5 +1,5 @@
 1+1
-using DrWatson ; @quickactivate :LatticeModels
+using LatticeModels
 pyplot(box=true,fontfamily="sans-serif",label=nothing,palette=ColorSchemes.tab10.colors[1:10],grid=false,markerstrokewidth=0,linewidth=1.3,size=(400,400),thickness_scaling = 1.5)
 
 ## Parameters
