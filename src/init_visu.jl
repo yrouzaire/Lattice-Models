d28cb165f4cc492a072e0ef2c2617d292bfdbb67
yrouzaire <rouzaire.ylann@gmail.com>
2022-09-11 14:27:13 +0200

    new names split and join for -1/2 defect

--- src/init_visu.jl
+++ src/init_visu.jl
@@ -27,8 +27,11 @@ function init_thetas(space;params)
     return float_type.(thetas)
 end
 
-function create_single_defect(L,x0=round(Int,L/2),y0=round(Int,L/2);q=1,type="source")
-    @assert (q > 0 && type in ["source","sink","clockwise","counterclockwise"]) || (q < 0 && type in ["convergent","divergent","threefold1","threefold2"])
+function create_single_defect(L,x0=round(Int,L/2),y0=round(Int,L/2);q=1,type)
+    condition1 = (q > 0 && type in ["source","sink","clockwise","counterclockwise"])
+    condition2 = (q < 0 && type in ["split","join","convergent","divergent","threefold1","threefold2"])
+    @assert condition1 || condition2
+
     thetas = zeros(L,L)
     for y in 1:L , x in 1:L
         # q > 0
@@ -37,10 +40,10 @@ function create_single_defect(L,x0=round(Int,L/2),y0=round(Int,L/2);q=1,type="so
         elseif type == "sink"                 thetas[x,y] = q * atan(y-y0,x-x0) + pi/2
         elseif type == "source"               thetas[x,y] = q * atan(y-y0,x-x0) - pi/2
         # q = -1/2 (when q = -1, I think they are all equivalent)
-        elseif type == "threefold1"           thetas[x,y] = q * atan(y-y0,x-x0)
-        elseif type == "threefold2"           thetas[x,y] = q * atan(y-y0,x-x0) + pi
-        elseif type == "divergent"            thetas[x,y] = q * atan(y-y0,x-x0) + pi/2
-        elseif type == "convergent"           thetas[x,y] = q * atan(y-y0,x-x0) - pi/2
+        elseif type in ["threefold1"]          thetas[x,y] = q * atan(y-y0,x-x0)
+        elseif type in ["threefold2"]          thetas[x,y] = q * atan(y-y0,x-x0) + pi
+        elseif type in ["divergent" ,"split"]     thetas[x,y] = q * atan(y-y0,x-x0) + pi/2
+        elseif type in ["convergent","join"]      thetas[x,y] = q * atan(y-y0,x-x0) - pi/2
         end
     end
     return thetas
@@ -52,10 +55,23 @@ function create_pair_vortices(L;r0=Int(L/2),q,type)
     @assert r0 ≤ 0.5L  "Error : r0 > L/2. "
     @assert iseven(r0) "Error : r0 has to be even. "
 
-    thetas = create_single_defect(L,round(Int,L/2+r0/2),round(Int,L/2),q=+q,type=type[1]) +
-             create_single_defect(L,round(Int,L/2-r0/2),round(Int,L/2),q=-q,type=type[2])
+    if isa(type,String)
+        if     type in ["pair1","source_split"] type_pos,type_neg = "source","threefold1"
+        elseif type in ["pair2","sink_join"]    type_pos,type_neg = "source","threefold2"
+        elseif type in ["pair3","cw_31"]        type_pos,type_neg = "source","split"
+        elseif type in ["pair4","ccw_32"]       type_pos,type_neg = "source","join"
+        else error("Type Unknown!")
+        end
+    elseif isa(type,Vector{String})
+        type_pos , type_neg = type
+    else error("Type Unknown!")
+    end
 
-    return smooth_border!(thetas)
+    thetas = create_single_defect(L,round(Int,L/2+r0/2),round(Int,L/2),q=+q,type=type_pos) +
+             create_single_defect(L,round(Int,L/2-r0/2),round(Int,L/2),q=-q,type=type_neg)
+
+             # return smooth_border!(thetas)
+    return (thetas)
 end
 
 function smooth_border!(thetas)
@@ -70,7 +86,7 @@ function smooth_border!(thetas)
             thetas[i,mod1(Int(cst-j+1),L)] = smoothed_values[j]
         end
     end
-    return thetas # in fact, this procedure is insensible to relax!() since already smooth
+    return thetas # in fact, this procedure is fairly insensible to relax!() since already smooth
 end
 
 function create_2pairs_vortices(L;r0,q,type)
@@ -87,8 +103,8 @@ end
 
 ## ------------------------ Visualization  ------------------------
 function plot_thetas(thetas::Matrix{<:AbstractFloat},model::AbstractModel,lattice::AbstractLattice;defects=false,title="",colorbar=true,cols = cgrad([:black,:blue,:green,:orange,:red,:black]),size=(485,400))
-    if model.symmetry == "nematic" modd = pi
-    elseif model.symmetry == "polar" modd = 2pi
+    if     model.symmetry == "nematic" modd = pi
+    elseif model.symmetry == "polar"   modd = 2pi
     end
     p = heatmap(mod.(thetas',modd),c=cols,clims=(0,modd),size=size,
         colorbar=colorbar,colorbartitle="θ",title=title,aspect_ratio=1)
