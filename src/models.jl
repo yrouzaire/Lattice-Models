771c727939bf67b56af43c0541725a0b24a52416
yrouzaire <rouzaire.ylann@gmail.com>
2022-09-22 13:42:51 +0200

    new AbstractPropagationModel and new PropagationForcedXY

--- src/models.jl
+++ src/models.jl
@@ -1,8 +1,8 @@
 using Parameters
 
-# export XY, ForcedXY, VisionXY
 
 abstract type AbstractModel{AbstractFloat} end
+abstract type AbstractPropagationModel{AbstractFloat} <: AbstractModel{AbstractFloat} end
 
 ## ---------------------------- Classical XY Model ----------------------------
 # Langevin
@@ -14,7 +14,6 @@ mutable struct XY{AbstractFloat} <: AbstractModel{AbstractFloat}
     rho::AbstractFloat
 
 end
-
 function XY(params)
     @unpack T,symmetry,dt,float_type,rho = params
     T,dt,rho = convert.(float_type,(T,dt,rho))
@@ -30,7 +29,6 @@ mutable struct MCXY{AbstractFloat} <: AbstractModel{AbstractFloat}
     rho::AbstractFloat
 
 end
-
 function MCXY(params)
     @unpack T,symmetry,float_type,rho = params
     T,rho = convert.(float_type,(T,rho))
@@ -93,4 +91,25 @@ function VisionXY(params)
     if vision ≠ 2π @assert symmetry == "polar" "I am not sure how to interpret a vision cone with nematic symmetry" end
 
     return VisionXY{float_type}(T,vision,symmetry,dt,zero(float_type),rho)
+end
+
+## Propagation Models
+mutable struct PropagationForcedXY{AbstractFloat} <: AbstractPropagationModel{AbstractFloat}
+    T::AbstractFloat
+    Var::AbstractFloat
+    symmetry::String
+    omegas::AbstractArray{AbstractFloat}
+    dt::AbstractFloat
+    t::AbstractFloat
+    rho::AbstractFloat
+end
+function PropagationForcedXY(params)
+    @unpack L,T,Var,symmetry,dt,float_type,rho  = params
+    T,Var,dt,rho = convert.(float_type,(T,Var,dt,rho))
+
+    omegas = AbstractArray{float_type}
+    # instanciated later during the init, when one knows the type of lattice (1D, 2D)
+
+    rho1 = one(float_type) # for now, we don't want to investigate holes in this model
+    return PropagationForcedXY{float_type}(T,Var,symmetry,omegas,dt,zero(float_type),rho1)
 end
