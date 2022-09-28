388e188834aa22cf89737a1ab3ccae66bafae658
yrouzaire <rouzaire.ylann@gmail.com>
2022-09-28 09:45:36 +0200

    Minor

--- parameters.jl
+++ parameters.jl
@@ -1,12 +1,12 @@
 # Physical Parameters
-L = 100
+L = 200
 T = 0.1
 symmetry = "polar"
 propulsion = "polar"
-Var = 0.2
-A = 0
+Var = 0.
+A = 3
 vision = 5π/3
-rho = 1
+rho = 0.95
 algo = "A" # rule for collision!() for model = MovingXY
 
 # Numerical Parameters
@@ -18,7 +18,7 @@ transients = 1E3
 every = 1E2
 
 # Initialisation
-init = "pair"
+init = "hightemp"
 q = 1
 r0 = Int(L/2)
 type1defect = "clockwise"
