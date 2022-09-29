d12ce31c1a423c7c78a99ca505925fe0ced0abae
yrouzaire <rouzaire.ylann@gmail.com>
2022-09-29 16:30:37 +0200

    cleans width_proposal, hides it from end user

--- scripts/cluster/parameters_cluster.jl
+++ scripts/cluster/parameters_cluster.jl
@@ -12,7 +12,6 @@ algo = "A" # rule for collision!() for model = MovingXY
 # Numerical Parameters
 dt = 1E-2
 float_type = Float32
-width_proposal = 2# dummy, hard coded in
 tmax = 2E3
 transients = 1E3
 every = 1E2
@@ -26,7 +25,7 @@ type2defect = "pair2"
 
 # Containers
 params_phys = Dict("L"=>L,"Ts"=>Ts,"Var"=>Var,"As"=>As,"rhos"=>rhos,"vision"=>vision,"symmetry"=>symmetry,"propulsion"=>propulsion,"algo"=>algo)
-params_num  = Dict("dt"=>dt,"float_type"=>float_type,"width_proposal"=>width_proposal,"tmax"=>tmax,"transients"=>transients)
+params_num  = Dict("dt"=>dt,"float_type"=>float_type,"tmax"=>tmax,"transients"=>transients)
 params_init = Dict("init"=>init,"q"=>q,"r0"=>r0,"type1defect"=>type1defect,"type2defect"=>type2defect)
 
 params = merge(params_num,params_phys,params_init)
