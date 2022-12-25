e43bd5bd810e44bc2e21a1e4e9a327e932607bd1
yrouzaire <rouzaire.ylann@gmail.com>
2022-12-25 22:17:35 +0100

    Minor

--- scripts/defects_ML/infer_mu_function.jl
+++ scripts/defects_ML/infer_mu_function.jl
@@ -11,12 +11,19 @@ CHARGE = 1
     @unpack base_dataset,mus,dµ = load("data/for_ML/base_dataset_µN1.jld2")
     base_datasetN = base_dataset;
 
+## Visually check the dataset
+# ind = 17#rand(1:length(mus))
+#     p=plot_thetas(base_datasetN[:,:,ind],model,lattice)
+#     display_quiver!(p,base_datasetN[:,:,ind],WINDOW)
+#     title!("µ = $(mus[ind])")
+
+## Functions
 function infer_mu(thetas;q,window=WINDOW,decay=true)
     if decay && q > 0 return infer_mu_decay(thetas,q=q,window=window)
-    else return infer_mu_0(thetas,q=q,window=window)
+    else return infer_mu_no_decay(thetas,q=q,window=window)
     end
 end
-function infer_mu_0(thetas::Matrix{T};q,window=WINDOW) where T<:AbstractFloat
+function infer_mu_no_decay(thetas::Matrix{T};q,window=WINDOW) where T<:AbstractFloat
     L = size(thetas,1)
     @assert L == 2window+1
     muss = zeros(size(thetas))
@@ -66,41 +73,41 @@ function infer_mu_decay(thetas::Matrix{T};q,window=WINDOW) where T<:AbstractFloa
 end
 
 ## Test noiseless
-# inferredP  = zeros(length(mus))
-#     inferredN = zeros(length(mus))
-#     decay = true
-#     for ind in 1:64
-#         inferredP[ind] = infer_mu(base_datasetP[:,:,ind],q=CHARGE,decay=decay)
-#         inferredN[ind] = infer_mu(base_datasetN[:,:,ind],q=-CHARGE,decay=decay)
-#     end
-#     p1 = plot(xlabel="True µ",ylabel="Inferred µ",legend=:top,title="Noiseless")
-#     plot!(mus,inferredP,m=true,c=:red,label="q=1")
-#     plot!(mus,inferredN,m=true,c=:green,label="q=-1")
-#     plot!(x->x,c=:black)
+inferredP  = zeros(length(mus))
+    inferredN = zeros(length(mus))
+    decay = true
+    for ind in 1:64
+        inferredP[ind] = infer_mu(base_datasetP[:,:,ind],q=CHARGE,decay=decay)
+        inferredN[ind] = infer_mu(base_datasetN[:,:,ind],q=-CHARGE,decay=decay)
+    end
+    p1 = plot(xlabel="True µ",ylabel="Inferred µ",legend=:top,title="Noiseless")
+    plot!(mus,inferredP,m=true,c=:red,label="q=1")
+    plot!(mus,inferredN,m=true,c=:green,label="q=-1")
+    plot!(x->x,c=:black)
 
 ## Test with noise
-# inferredP  = zeros(length(mus))
-#     inferredN = zeros(length(mus))
-#     noise = 0.3randn(W21,W21,64)
-#     decay = false
-#     for ind in 1:64
-#         inferredP[ind]  = infer_mu(base_datasetP[:,:,ind] + noise[:,:,ind],q=CHARGE,decay=decay)
-#         inferredN[ind] = infer_mu(base_datasetN[:,:,ind]+ noise[:,:,ind],q=-CHARGE,decay=decay)
-#     end
-#     p2 = plot(xlabel="True µ",ylabel="Inferred µ",legend=:top,title="Noisy (without decay)")
-#     plot!(mus,inferredP,line=false,m=true,c=:red,label="q=1")
-#     plot!(mus,inferredN,line=false,m=true,c=:green,label="q=-1")
-#     plot!(x->x,c=:black)
-#
-# decay = true
-#     for ind in 1:64
-#         inferredP[ind]  = infer_mu(base_datasetP[:,:,ind] + noise[:,:,ind],q=CHARGE,decay=decay)
-#         inferredN[ind] = infer_mu(base_datasetN[:,:,ind]+ noise[:,:,ind],q=-CHARGE,decay=decay)
-#     end
-#     p3 = plot(xlabel="True µ",ylabel="Inferred µ",legend=:top,title="Noisy (with decay for q=1)")
-#     plot!(mus,inferredP,line=false,m=true,c=:red,label="q=1")
-#     plot!(mus,inferredN,line=false,m=true,c=:green,label="q=-1")
-#     plot!(x->x,c=:black)
+inferredP  = zeros(length(mus))
+    inferredN = zeros(length(mus))
+    noise = 0.3randn(W21,W21,64)
+    decay = false
+    for ind in 1:64
+        inferredP[ind] = infer_mu(base_datasetP[:,:,ind] + noise[:,:,ind],q=CHARGE,decay=decay)
+        inferredN[ind] = infer_mu(base_datasetN[:,:,ind]+ noise[:,:,ind],q=-CHARGE,decay=decay)
+    end
+    p2 = plot(xlabel="True µ",ylabel="Inferred µ",legend=:top,title="Noisy (without decay)")
+    plot!(mus,inferredP,line=false,m=true,c=:red,label="q=1")
+    plot!(mus,inferredN,line=false,m=true,c=:green,label="q=-1")
+    plot!(x->x,c=:black)
+
+decay = true
+    for ind in 1:64
+        inferredP[ind] = infer_mu(base_datasetP[:,:,ind] + noise[:,:,ind],q=CHARGE,decay=decay)
+        inferredN[ind] = infer_mu(base_datasetN[:,:,ind] + noise[:,:,ind],q=-CHARGE,decay=decay)
+    end
+    p3 = plot(xlabel="True µ",ylabel="Inferred µ",legend=:top,title="Noisy (with decay for q=1)")
+    plot!(mus,inferredP,line=false,m=true,c=:red,label="q=1")
+    plot!(mus,inferredN,line=false,m=true,c=:green,label="q=-1")
+    plot!(x->x,c=:black)
 #
 #
 # plot(p1,p2,p3,size=(1200,400),layout=(1,3))
